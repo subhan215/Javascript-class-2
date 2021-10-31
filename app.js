67788d7d5c66565ed6bedfd74ebbc8f7c3c54b2a
subhan215 <63653805+subhan215@users.noreply.github.com>
2021-10-31 08:47:10 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,119 @@
+// Concatenation
+/*  var str1 = "My name is ";
+ var str2 = "Subhan Rangila";
+ alert(str1 + str2); */
+
+/* var num1 = 19448;
+var num2  = 374857;
+alert(num1 + num2) */
+// if we write number in without then it will perform basic plus operation
+/* var num1 = "584849";
+var num2 = "839782";
+alert(num1 + num2); */
+// if we write number in double quotations then it will concatenate them instead of adding
+
+// Mathematical Operators
+// Plus
+/* var num = 10
+var newNum = num + 200;
+alert(newNum); */
+// Minus
+/* var num1 = 80
+var num2 = 60
+var newNum =  num1 - num2;
+alert(newNum); */
+// Divide 
+/*  var num1 = 288
+var num2 = 9
+var newNum =  num1 / num2;
+alert(newNum);  */
+// Multiply
+/* var num1 = 290
+var num2 = 9
+var newNum =  num1 * num2;
+alert(newNum);  */
+// Modulas
+/* var num1 = 285
+var num2 = 7
+var newNum =  num1 % num2;
+alert(newNum);  */
+
+// Increment and Decrement
+// Post Increment
+/* var num = 1;
+var newNum = num++;
+alert(newNum);
+alert(num); */
+// Pre Increment
+/* var num = 1;
+ var newNum = ++num;
+ alert(newNum);
+ alert(num)
+ */
+// Post Decrement
+/* var num = 1;
+ var newNum = num--;
+alert(newNum);
+alert(num);
+ */
+// Pre Decrement
+/* var num = 1;
+var newNum = --num;
+alert(num);
+alert(newNum); */
+// Increment and Decrement Operations
+// Increment
+/* var num = 13;
+var newNum = ++num - num-- + num++ + --num - ++num
+             // 14 -  14    + 13   +    13 -  14
+             // = 12
+ alert(newNum)  */
+
+
+// Marksheet
+var name = prompt("Enter your name  ", "Name: Subhan");
+document.write(name);
+document.write("<br>")
+
+var rollNo = prompt("Enter your roll no ", "Roll No : 393");
+document.write(rollNo);
+document.write("<br>");
+
+var college = "College: Adamjee Govt. Science College";
+document.write(college);
+document.write("<br>");
+
+var engmarks = +prompt("Enter your english marks", "80");
+document.write("Eng:" + engmarks);
+document.write("<br>");
+
+var urdumarks = +prompt("Enter your urdu marks", "75");
+document.write("Urdu: " + urdumarks);
+document.write("<br>");
+
+var islmarks = +prompt("Enter your islamiat marks", "85");
+document.write("Isl: " + islmarks);
+document.write("<br>");
+
+var jsmarks = +prompt("Enter your javascript marks", "100");
+document.write("Js: " + jsmarks);
+document.write("<br>");
+
+var htmlmarks = +prompt("Enter your html marks", "95");
+document.write("Html: " + htmlmarks);
+document.write("<br>");
+
+var obtainmarks = engmarks + urdumarks + islmarks + jsmarks + htmlmarks;
+var obtain = "Obtain Marks: " + obtainmarks;
+document.write(obtain);
+document.write("<br>");
+
+var total = "Total: " + 500;
+document.write(total);
+document.write("<br>");
+
+var percentagename = "Percentage : ";
+document.write(percentagename);
+
+var percentage = (obtainmarks / 500) * 100;
+document.write(percentage);
